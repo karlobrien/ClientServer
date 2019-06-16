0415bb6f977c45867abfefba4727e6283b0605b4
Karl O'Brien <karlobrien@gmail.com>
2019-06-16 20:58:01 +0100

    simple service

---
+++ src/Native.Grpc/SimpleImpl.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Simple;
+
+namespace Native.Grpc
+{
+    public class SimpleImpl : Better.BetterBase
+    {
+        public override Task<BetResponse> PlaceBet(Bet bet, ServerCallContext context)
+        {
+            return Task.FromResult(new BetResponse {
+                Id = 1, ClientId = 1, Status = BetResponse.Types.Status.Ack
+            });
+        }
+
+        public override async Task RaceStatus(Empty request, IServerStreamWriter<RaceStream> responseStream, ServerCallContext context)
+        {
+            RaceStream rs = new RaceStream();
+            RacePosition rp1 = new RacePosition();
+            await responseStream.WriteAsync(rs);
+        }
+    }
+}
